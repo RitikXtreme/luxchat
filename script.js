97d3270f31c936f633bf2badba2ebc86e0269795
RitikXtreme <rairithik80@gmail.com>
2026-07-04 07:10:21 +0530

    Implement chat functionality with Supabase

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+const SUPABASE_URL = "https://rxundroadkzdxzszqoai.supabase.co";
+const SUPABASE_KEY = "sb_publishable_yRqIMmEfIvfyBbHS7oDJHw_w5ngE3Uk";
 
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const username = document.getElementById("username");
+const message = document.getElementById("message");
+const sendBtn = document.getElementById("sendBtn");
+const chatBox = document.getElementById("chatBox");
+
+async function loadMessages() {
+    const { data, error } = await client
+        .from("messages")
+        .select("*")
+        .order("created_at", { ascending: true });
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    chatBox.innerHTML = "";
+
+    data.forEach(addMessage);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function addMessage(msg) {
+    const div = document.createElement("div");
+    div.className = "message";
+
+    div.innerHTML = `
+        <strong>${msg.username}</strong>
+        ${msg.message}
+    `;
+
+    chatBox.appendChild(div);
+}
+
+sendBtn.onclick = async () => {
+
+    if(username.value.trim()==="" || message.value.trim()===""){
+        alert("Enter your name and message");
+        return;
+    }
+
+    const { error } = await client
+        .from("messages")
+        .insert({
+            username: username.value,
+            message: message.value
+        });
+
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    message.value="";
+};
+
+client
+.channel("chat-room")
+.on(
+    "postgres_changes",
+    {
+        event:"INSERT",
+        schema:"public",
+        table:"messages"
+    },
+    payload=>{
+        addMessage(payload.new);
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }
+)
+.subscribe();
+
+loadMessages();
